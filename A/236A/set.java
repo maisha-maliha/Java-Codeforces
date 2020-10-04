644004ba01ab30de36c216922f183bf78829d827
maisha-maliha <officiallymaisha@gmail.com>
2020-10-04 20:55:45 +0600

    solved 2

---
+++ A/236A/set.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class set{
+    public static void main(String args[]) {
+        Scanner inp = new Scanner(System.in);
+        String str = inp.nextLine();
+        String str2="";
+        byte i,j,k;
+        for(i=0;i<str.length();i++){
+          k = (byte)(str2.length());
+          for(j=0;j<k;j++){
+            if(str2.charAt(j) == str.charAt(i)){
+              break;
+            }
+          }
+          if(j==k){
+            str2 += str.charAt(i);
+          }
+        }
+        if(str2.length()%2==0){
+          System.out.println("CHAT WITH HER!");
+        } else {
+          System.out.println("IGNORE HIM!");
+        }
+        inp.close();
+    }
+}
---
+++ A/339A/program.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+public class program{
+    public static void main(String args[]) {
+        Scanner inp = new Scanner(System.in);
+        String val = inp.nextLine();
+        String val2="";
+        int j , i;
+        for(i=0;i<val.length();i++){
+            if(val.charAt(i)=='1'){
+                if(val2.length()==0){
+                    val2+="1";
+                } else {
+                    val2+="+1";
+                }
+            }
+        }
+        for(i=0;i<val.length();i++){
+            if(val.charAt(i)=='2'){
+                if(val2.length()==0){
+                    val2+="2";
+                } else {
+                    val2+="+2";
+                }
+            }
+        }
+        for(i=0;i<val.length();i++){
+            if(val.charAt(i)=='3'){
+                if(val2.length()==0){
+                    val2+="3";
+                } else {
+                    val2+="+3";
+                }
+            }
+        }
+        System.out.println(val2);
+        inp.close();
+    }
+}
